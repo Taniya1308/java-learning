be20908872b4eac14e4d956bd203d5c470ad0b02
Taniya1308 <181471826+Taniya1308@users.noreply.github.com>
2026-09-10 09:41:30 +0530

    Change name variable from 'Taniya' to 'Tannu'

--- 01-Basics/02-Variables/VariablePractice.java
+++ 01-Basics/02-Variables/VariablePractice.java
@@ -1,7 +1,7 @@
 public class VariablePractice{
     public static void main(String[] args){
         int age=21;
-        String name="Taniya";
+        String name="Tannu";
         String collegeName="PSIT";
         float CGPA=79.25f;
 
